9bd08d2d052009d6bfa3b9572b2820f3c9230129
moritzs <mo.schiesser@service7000.ch>
2021-03-09 16:15:39 +0100

    playing around with scottplot a bit

--- gttcharts/Program.cs
+++ gttcharts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using gttcharts.Models;
+using ScottPlot;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,6 +18,53 @@ namespace gttcharts
                 issues = context.Issues.ToList();
                 records = context.Records.ToList();
             }
+
+            var query = from i in issues
+                        join r in records
+                        on i.Iid equals r.Iid
+                        into list
+                        select new
+                        {
+                            Issue = i,
+                            Records = list
+                        };
+
+            foreach (var row in query)
+            {
+                Console.WriteLine($"Issue: {row.Issue.Title}, estimated {row.Issue.TotalEstimate}h, spent {row.Issue.Spent}");
+                var q2 = from r in row.Records
+                         group r by r.User
+                         into lst
+                         select new
+                         {
+                             User = lst.Key,
+                             Spent = lst.Sum(r => r.Time)
+                         };
+                foreach (var usrTime in q2)
+                {
+                    Console.WriteLine($"{usrTime.User} spent {usrTime.Spent}h on this issue");
+                }
+                Console.WriteLine("=========");
+            }
+
+            var plt = new ScottPlot.Plot();
+            var perMilestone = from i in issues
+                               group i by i.Milestone
+                               into lst
+                               select new
+                               {
+                                   Milestone = lst.Key,
+                                   Estimate = lst.Sum(i => i.TotalEstimate),
+                                   Spent = lst.Sum(i => i.Spent)
+                               };
+
+            string[] milestonenames = perMilestone.Select(i => i.Milestone).ToArray();
+            double[] estimates = perMilestone.Select(p => p.Estimate).ToArray();
+            double[] spent = perMilestone.Select(p => p.Spent).ToArray();
+            plt.PlotBarGroups(milestonenames, new string[] { "estimated", "spent" }, new double[][] { estimates, spent }, showValues: true);
+            plt.Legend(location: legendLocation.upperRight);
+            plt.SaveFig("PerMilestone.png");
+
             Console.Read();
         }
     }
